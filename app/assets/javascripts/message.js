8b43d40686bf5f36f75c9f22bbd488e3da0ea100
DON4024 <61001693+DON4024@users.noreply.github.com>
2020-02-24 20:47:05 +0900

    create ajax-feature

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,67 @@
+$(function(){ 
+  function buildHTML(message){
+   if ( message.image == "null" ) {
+     var html =
+      `<div class="message-group">
+        <div class="message-group__text-items">
+          <div class="message-group__text-items__name">
+            ${message.user_name}
+          </div>
+          <div class="message-group__text-items__date">
+            ${message.created_at}
+          </div>
+        </div>
+        <div class="message-group__text-box">
+          <p class="lower-message__content">
+            ${message.content}
+          </p>
+          
+        </div>
+      </div>`
+     return html;
+   } else {
+     var html =
+      `<div class="message-group">
+        <div class="message-group__text-items">
+          <div class="message-group__text-items__name">
+            ${message.user_name}
+          </div>
+          <div class="message-group__text-items__date">
+            ${message.created_at}
+          </div>
+        </div>
+        <div class="message-group__text-box">
+          <p class="lower-message__content">
+            ${message.content}
+          </p>
+          <img src=${message.image.url} >
+        </div>
+      </div>`
+     return html;
+   };
+ }
+$('#new_message').on('submit', function(e){
+  e.preventDefault();
+  var formData = new FormData(this);
+  var url = $(this).attr('action')
+  $.ajax({
+    url: url,
+    type: "POST",
+    data: formData,
+    dataType: 'json',
+    processData: false,
+    contentType: false
+  })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.chat-main__message-list').append(html);
+      $('.chat-main__message-list').animate({ scrollTop: $('.chat-main__message-list')[0].scrollHeight});
+      $(".submit-btn").removeAttr("disabled");
+      $('form')[0].reset();
+    })
+    .fail(function() {
+      alert("メッセージ送信に失敗しました");
+  });
+})
+});
+
